a2b52fa37927093d6a1ac241584a035b7d652eed
Anatol Kaczmarek <92335424+Tole-k@users.noreply.github.com>
2026-05-22 22:14:18 +0200

    cleanup + scripts

---
+++ scripts/train_3_nets.sh
@@ -0,0 +1,3 @@
+uv run nb201-train-cifar100 "(2, 0, 0, 4, 2, 2)"  --epochs 200 --batch-size 256 --data-root data
+uv run nb201-train-cifar100 "(3, 3, 0, 2, 4, 2)"  --epochs 200 --batch-size 256 --data-root data
+uv run nb201-train-cifar100 "(2, 3, 0, 1, 3, 0)"  --epochs 200 --batch-size 256 --data-root data
